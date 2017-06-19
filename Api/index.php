df0aada14d09501b3ff0894608570ca4837106c0
Aleksandar Zivanovic <aleksandar.zivanovic@sbgenomics.com>
2017-06-19 13:33:27 +0200

    upd

--- Api/index.php
+++ Api/index.php
@@ -9,20 +9,19 @@
 require_once __DIR__ . '/../Core/loader.php';
 
 $handlers = [
-    'user' => [
-        'test' => [
-            'login' => \Api\Handlers\User\LoginActionHandler::getSharedInstance()
+    'user' => [ // Group "user"
+        'test' => [ // Sub-Grup "test"
+            'login' => \Api\Handlers\User\LoginActionHandler::getSharedInstance() // Action "user.test.login"
         ],
-        'register' => \Api\Handlers\User\RegisterActionHandler::getSharedInstance()
+        'register' => \Api\Handlers\User\RegisterActionHandler::getSharedInstance() // Action "user.register"
     ],
-    'idea' => [
-        'create' => \Api\Handlers\Idea\CreateActionHandler::getSharedInstance(),
-        'update' => \Api\Handlers\Idea\UpdateActionHandler::getSharedInstance(),
-        'list' => \Api\Handlers\Idea\ListActionHandler::getSharedInstance()
+    'idea' => [ // Group "idea"
+        'create' => \Api\Handlers\Idea\CreateActionHandler::getSharedInstance(), // Action "idea.create"
+        'update' => \Api\Handlers\Idea\UpdateActionHandler::getSharedInstance(), // Action "idea.update"
+        'list' => \Api\Handlers\Idea\ListActionHandler::getSharedInstance() // Action "idea.list"
     ]
 ];
 
 \Core\Libs\Application\Application::getSharedInstance(__DIR__ . '/config.json')
     ->register($handlers)
-    ->run()
-;
+    ->run();
